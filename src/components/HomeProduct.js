c16b89dfa66c6b91baf9e08fbbd163d7dd390f72
Amaru333 <amrutesharun0599@gmail.com>
2020-12-23 20:51:32 +0530

    Added homepage

---
+++ src/components/HomeProduct.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/homeproduct.css';
+
+function HomeProduct() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default HomeProduct
