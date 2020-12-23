3625ae37569c6faea8f9d8577a53a34a1ff9fe5f
Amaru333 <amrutesharun0599@gmail.com>
2020-12-23 20:13:44 +0530

    Initialized

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../style/header.css'
+import Logo from '../images/logo.png'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import SearchIcon from '@material-ui/icons/Search';
+
+function Header() {
+    return (
+        <div className="header">
+            <img src={Logo} alt="logo" className="logo" />
+            <div className="headerSearch">
+                <input className="searchBar" type="text" />
+                <SearchIcon className="headerSearchIcon" fontSize="large"/>
+            </div>
+            <div className="headerOptions">
+                <span className="lineOne">Hello</span>
+                <span className="lineTwo">Sign In</span>
+            </div>
+            <div className="headerOptions">
+                <span className="lineOne">Returns</span>
+                <span className="lineTwo">& Orders</span>
+            </div>
+            <div className="headerOptions2">
+                <ShoppingCartIcon />
+                <p>Cart</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBAM9F-q6jLiKLCjx_dfDIPCGooEqrY2Go",
+    authDomain: "clone-cc659.firebaseapp.com",
+    projectId: "clone-cc659",
+    storageBucket: "clone-cc659.appspot.com",
+    messagingSenderId: "1063816342239",
+    appId: "1:1063816342239:web:56373dec3981d6466a576a",
+    measurementId: "G-L5PKP1QBVV"
+  };
