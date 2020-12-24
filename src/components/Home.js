3efda8bb51c79b10f4db22814ead20ee431b80cb
Amaru333 <amrutesharun0599@gmail.com>
2020-12-24 11:50:58 +0530

    Home page product added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import '../style/home.css';
+import HomeProduct from './HomeProduct';
 
 function Home() {
     return (
         <div className="home">
             <div className="homeContainer">
-                <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/HeroDec-20/Dec_AuCC_Seasonal_1X._CB414979991_.jpg" alt="homepage" className="imageContainer"/>
-            </div>
-            <div className="ProductLine">
-                {/*HomeProduct*/}
-                {/*HomeProduct*/}
-            </div>
-            <div className="ProductLine">
-                {/*HomeProduct*/}
-                {/*HomeProduct*/}
-                {/*HomeProduct*/}                
-            </div>
-            <div className="ProductLine">
-                {/*HomeProduct*/}
+                <div className="maskImage">
+                    <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/HeroDec-20/Dec_AuCC_Seasonal_1X._CB414979991_.jpg" alt="homepage" className="imageContainer"/>
+                </div>
+                <div className="ProductLine">
+                    <HomeProduct />
+                    <HomeProduct />
+                </div>
+                <div className="ProductLine">
+                    <HomeProduct />
+                    <HomeProduct />
+                    <HomeProduct />               
+                </div>
+                <div className="ProductLine">
+                    <HomeProduct />
+                </div>
             </div>
         </div>
     )
--- src/components/HomeProduct.js
+++ src/components/HomeProduct.js
@@ -3,8 +3,21 @@ import '../style/homeproduct.css';
 
 function HomeProduct() {
     return (
-        <div>
-            
+        <div className="homeProduct">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/71dGb%2B7qhQL._SL1500_.jpg" alt="Samsung Curved Monitor" className="homeProductImage" />
+            <div className="homeProductInfo">
+                <p className="homeProductTitle">Samsung 24inch Curved monitor with AMD Freesync, 144hz refresh rate and Full HD resolution (1920x1080 pixels) with HDMI cable</p>
+                <div className="homeProductPrice">
+                    <small>₹</small>
+                    <strong>13,999</strong>
+                </div>
+                <div className="homeProductRating">
+                    <p>⭐</p>
+                    <p>⭐</p>
+                    <p>⭐</p>
+                </div>
+            </div>
+            <button className="homeAddToCart">Add to cart</button>
         </div>
     )
 }
