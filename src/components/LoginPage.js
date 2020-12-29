b978cd609c21cb33fd4af66e83d6d46442129552
Amaru333 <amrutesharun0599@gmail.com>
2020-12-29 21:27:20 +0530

    Added backend support

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../images/amazon-logo-transparent.png';
 import '../style/LoginPage.css';
 import { Link } from 'react-router-dom';
 import {Helmet} from "react-helmet";
+// import Axios from 'axios';
 
 function LoginPage() {
+
+    const displayInfo = () => {
+        console.log(loginMail + loginPassword);
+    };
+
+    const [loginMail, setLoginMail] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+
     return (
         <div className="loginPage">
             <Helmet>
@@ -20,10 +29,14 @@ function LoginPage() {
                 <div className="innerElements">
                     <h1>Login</h1>
                     <p>Enter your email id</p>
-                    <input type="field" className="loginInput" />
+                    <input type="field" className="loginInput" onChange={(e) => {
+                        setLoginMail(e.target.value);
+                    }} />
                     <p>Enter your password</p>
-                    <input type="field" className="loginInput" />
-                    <button className="loginButton">Continue</button>
+                    <input type="password" className="loginInput" onChange={(e) => {
+                        setLoginPassword(e.target.value);
+                    }} />
+                    <button className="loginButton" onClick={displayInfo} >Continue</button>
                     <span className="terms">By logging in you are agreeing to the Terms and Conditions</span>
                 </div>
             </div>
@@ -31,14 +44,14 @@ function LoginPage() {
                 <div className="aDivider">
                     <h5><span className="line">New to Amazon?</span></h5>
                 </div>
-                <Link to="/register">
-                    <div className="registerationButton">
-                        <button className="registerButton">Create a new Amazon account</button>
-                    </div>
-                </Link>
+                <div className="registerationButton">
+                    <Link to="/register">
+                        <button className="registerButton" >Create a new Amazon account</button>
+                    </Link>
+                </div>
             </div>
         </div>
     )
 }
 
-export default LoginPage
+export default LoginPage;
--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../images/amazon-logo-transparent.png';
 import '../style/LoginPage.css';
 import { Link } from 'react-router-dom';
 import {Helmet} from "react-helmet";
+import Axios from 'axios';
 
 function RegisterPage() {
+
+    const [registerName, setRegisterName] = useState("");
+    const [registerMail, setRegisterMail] = useState("");
+    const [registerMobile, setRegisterMobile] = useState("");
+    const [registerPassword, setRegisterPassword] = useState("");
+    
+    const registerUser = () => {
+        Axios.post('http://localhost:3001/createUser', {
+            name: registerName,
+            mail: registerMail,
+            mobile: registerMobile,
+            password: registerPassword
+        }).then(() => {
+            console.log("Success");
+        });
+    };
+
     return (
         <div className="registerPage">
             <Helmet>
@@ -20,16 +38,24 @@ function RegisterPage() {
                 <div className="innerElements">
                     <h1>Create a new account</h1>
                     <p>Enter your name</p>
-                    <input type="field" className="loginInput"/>
+                    <input type="field" className="loginInput" onChange={(e) => {
+                        setRegisterName(e.target.value);
+                    }} />
                     <p>Enter your email id</p>
-                    <input type="field" className="loginInput"/>
+                    <input type="field" className="loginInput" onChange={(e) => {
+                        setRegisterMail(e.target.value);
+                    }} />
                     <p>Enter your mobile number</p>
-                    <input type="field" className="loginInput" />
+                    <input type="field" className="loginInput" onChange={(e) => {
+                        setRegisterMobile(e.target.value);
+                    }} />
                     <p>Enter your password</p>
-                    <input type="password" className="loginInput" />
+                    <input type="password" className="loginInput" onChange={(e) => {
+                        setRegisterPassword(e.target.value);
+                    }} />
                     <p>Confirm password</p>
                     <input type="password" className="loginInput" />
-                    <button className="loginButton">Create an account</button>
+                    <button className="loginButton" onClick={registerUser}>Create an account</button>
                     <span className="terms">By creating an account in our website, you are agreeing to the Terms and Conditions</span>
                 </div>
             </div>
@@ -37,11 +63,11 @@ function RegisterPage() {
                 <div className="aDivider">
                     <h5><span className="line">Already have an account?</span></h5>
                 </div>
-                <Link to="/login">
-                    <div className="registerationButton">
+                <div className="registerationButton">
+                    <Link to="/login">
                         <button className="registerButton">Click here to Sign In</button>
-                    </div>
-                </Link>
+                    </Link>
+                </div>
             </div>
         </div>
     )
